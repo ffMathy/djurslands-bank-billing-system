d63dd172676e259bc47a8509d65258c71398305a
ffMathy <mathias.lorenzen@live.com>
2014-08-23 12:39:39 +0200

    initial commit and structure.

---
+++ Billing/Billing/ReviewForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Billing
+{
+    public partial class ReviewForm : Form
+    {
+        public ReviewForm()
+        {
+            InitializeComponent();
+
+            webBrowser.ScriptErrorsSuppressed = true;
+            webBrowser.Url = new Uri("https://portal4.djurslandsbank.dk/wps/bankdata/jsp/html/da/PortalFrame.jsp?danid=true");
+
+            webBrowser.DocumentCompleted += webBrowser_DocumentCompleted;
+        }
+
+        void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser.DocumentCompleted -= webBrowser_DocumentCompleted;
+
+        }
+    }
+}
